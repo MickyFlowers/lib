7423ac0ea14f5e3e1a2be8efd2f8f07518417c96
cyx <cyx010402@gmail.com>
2024-11-21 13:51:07 +0800

    add setup.py to install package

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xlib",
+    version="0.1.0",
+    packages=find_packages(where="."),
+    install_requires=[
+        "pyrealsense2==2.54.1.5216",
+        "numpy",
+        "opencv-python",
+        "matplotlib",
+        "scipy",
+    ],
+    entry_points={
+        "console_scripts": [],
+    },
+    url="https://github.com/MickyFlowers/xlib.git",
+    author="Yuxi Chen",
+    author_email="cyx010402@gmail.com",
+    description="lib of x",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
