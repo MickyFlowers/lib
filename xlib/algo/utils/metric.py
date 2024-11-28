74955b343a0ce44f7510f17bbb9d995b0406e115
cyx <cyx010402@gmail.com>
2024-11-28 11:19:02 +0800

    calculate psnr

--- xlib/algo/utils/metric.py
+++ xlib/algo/utils/metric.py
@@ -1,7 +1,31 @@
 import skimage.metrics
 import numpy as np
+import torch
+from typing import Union
+import torch.nn.functional as F
+
 
 def calc_ssim(img1: np.ndarray, img2: np.ndarray) -> float:
     assert img1.shape == img2.shape, "Image shapes do not match"
     score = skimage.metrics.structural_similarity(img1, img2, channel_axis=2)
     return score
+
+
+def calc_psnr(
+    img1: Union[np.ndarray, torch.Tensor],
+    img2: Union[np.ndarray, torch.Tensor],
+    max_pixel=1.0,
+    reduction="sum",
+) -> float:
+    if isinstance(img1, torch.Tensor) and isinstance(img2, torch.Tensor):
+        mse = F.mse_loss(img1, img2, reduction=reduction)
+        if mse == 0:
+            return float("inf")
+        psnr = 20 * torch.log10(max_pixel / torch.sqrt(mse))
+        return psnr.item()
+    elif isinstance(img1, np.ndarray) and isinstance(img2, np.ndarray):
+        mse = np.mean((img1 - img2) ** 2)
+        if mse == 0:
+            return float("inf")
+        psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
+        return psnr
